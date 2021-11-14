6ce0bbcb1245b7cbcb390e43321ebf63efa982e4
tom hsieh <cloudtom@gmail.com>
2021-11-14 19:18:17 +0800

    UPDT  jwt authentication settings

--- Startup.cs
+++ Startup.cs
@@ -13,9 +13,15 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using HAdmin.Data;
 using HAdmin.Repositories;
 using HAdmin.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace HAdmin
 {
@@ -37,12 +43,43 @@ namespace HAdmin
             services.AddScoped<IDataContext>(provider => provider.GetService<DataContext>());
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IHasher, Hasher>();
+            services.AddScoped<ITokenManager, TokenManager>();
 
+
+            services.AddHttpContextAccessor();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    //other settings
+                    ClockSkew = TimeSpan.Zero
+                };
+                options.Events = new JwtBearerEvents();
+                options.Events.OnMessageReceived = context =>
+                {
+                    if (context.Request.Cookies.ContainsKey("APP-AUTH-COOKIE"))
+                    {
+                        context.Token = context.Request.Cookies["APP-AUTH-COOKIE"];
+                    }
+                    return Task.CompletedTask;
+                };
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddCors();
+            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddControllers();
-            services.AddSpaStaticFiles(config =>
- {
-     config.RootPath = "wwwroot";
- });
+
             services.AddSwaggerDocument(config =>
             {
                 config.PostProcess = document =>
@@ -58,6 +95,10 @@ namespace HAdmin
                     };
                 };
             });
+            services.AddSpaStaticFiles(config =>
+{
+    config.RootPath = "wwwroot";
+});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -107,8 +148,20 @@ namespace HAdmin
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseCors(x =>
+            {
+                var origin = new string[] {
+                    "https://localhost:5001", "http://localhost:3000", "http://localhost:3001",
+                };
+                x
+                .WithOrigins(origin)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
