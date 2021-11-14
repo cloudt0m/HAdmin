b6489d63171908afa05c0aeffeec1b7051101346
tom hsieh <cloudtom@gmail.com>
2021-11-14 19:18:45 +0800

    UPDT add authorize to protect user api

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using HAdmin.Repositories;
 using HAdmin.Models;
 using HAdmin.Dtos;
 using HAdmin.Security;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HAdmin.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
