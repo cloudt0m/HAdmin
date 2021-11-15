5b09d2aa653e0b147762bec987eb494ba6d42308
tom hsieh <cloudtom@gmail.com>
2021-11-15 10:23:42 +0800

    SCAFFOLD other files

--- ClientApp/src/main.js
+++ ClientApp/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- ClientApp/vue.config.js
+++ ClientApp/vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   outputDir: '../wwwroot',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
 };
